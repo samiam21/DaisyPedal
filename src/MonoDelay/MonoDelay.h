4c33ec3632e0cc359949613351cae5c969c8c54c
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2020-10-16 21:51:29 -0400

    DP-14 - Add level control

--- src/MonoDelay/MonoDelay.h
+++ src/MonoDelay/MonoDelay.h
@@ -13,7 +13,7 @@
  * Button 3 - N/U
  * 
  * Knob 1 - Decay
- * Knob 2 - First Hit Level
+ * Knob 2 - Effect Level
  * Knob 3 - Mix
  * 
  * LED 1 - N/U
@@ -24,28 +24,43 @@
 // Pin renaming
 static const int tapTempoButtonPin = effectButtonPin1;
 static const int decayKnobPin = effectKnobPin1;
+static const int levelKnobPin = effectKnobPin2;
 
 // Constant parameters
 static const int audioInChannel = 0;
 static const int audioOutChannel = 0;
 static const size_t delayMaxSize = 96000;
 
+// Mutable parameters
+static DelayLine<float, delayMaxSize> del_line;
+
 // Tap tempo constants
 static const long tapTempoDebounce = 300;
+
+// Tap tempo mutables
+static size_t tempoBpm = 90;
+static unsigned long tapTempoTime = 0;
+static TempoArray tempoArray;
 
 // Decay constants
 static const int decayKnobFlutter = 10;
 static const int minDecayKnobValue = 0;
 static const int maxDecayKnobValue = 1024;
-static const float maxDecayValue = 0.75;
+static const float maxDecayValue = 0.75f;
 
-// Mutable parameters
-static size_t tempoBpm = 90;
-static unsigned long tapTempoTime = 0;
-static TempoArray tempoArray;
-static DelayLine<float, delayMaxSize> del_line;
+// Decay mutables
 static int decayKnobReading = 0;
 static float decayValue = 0.5f;
+
+// Level constants
+static const int levelKnobFlutter = 10;
+static const int minLevelKnobValue = 0;
+static const int maxLevelKnobValue = 1024;
+static const float maxLevelValue = 1.0f;
+
+// Level mutables
+static int levelKnobReading = 0;
+static float levelValue = 0.5f;
 
 // Function definitions
 extern void MonoDelaySetup();
@@ -57,5 +72,7 @@ extern void MonoDelayLoop();
 void TapTempoLoopControl();
 void DecayLoopControl();
 void SetDecayValue(int knobReading);
+void LevelLoopControl();
+void SetLevelValue(int knobReading);
 
 #endif
