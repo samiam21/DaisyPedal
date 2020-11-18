43bde03be429a12e4e67c7e2969c8072cfcd55ae
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2020-11-18 13:19:53 -0500

    Updating pinout to match new pcb

--- include/PedalConfig.h
+++ include/PedalConfig.h
@@ -11,33 +11,33 @@
 const int controlLedPin = LED_BUILTIN; // Built in LED is LED_BUILTIN
 
 // Pin Definitions - Selector
-const int effectSelectorPin1 = 0;
-const int effectSelectorPin2 = 1;
-const int effectSelectorPin3 = 2;
-const int effectSelectorPin4 = 3;
+const int effectSelectorPin1 = 29;
+const int effectSelectorPin2 = 28;
+const int effectSelectorPin3 = 27;
+const int effectSelectorPin4 = 26;
 
 // Pin Definitions - SPST
-const int effectSPSTPin1 = 27;
-const int effectSPSTPin2 = 28;
-const int effectSPSTPin3 = 29;
-const int effectSPSTPin4 = -1;
+const int effectSPSTPin1 = 6;
+const int effectSPSTPin2 = 7;
+const int effectSPSTPin3 = 8;
+const int effectSPSTPin4 = 5;
 
 // Pin Definitions - SPDT
-const int effectSPDT1Pin1 = 23;
-const int effectSPDT1Pin2 = 24;
-const int effectSPDT2Pin1 = -1;
-const int effectSPDT2Pin2 = -1;
+const int effectSPDT1Pin1 = 12;
+const int effectSPDT1Pin2 = 13;
+const int effectSPDT2Pin1 = 10;
+const int effectSPDT2Pin2 = 11;
 
 // Pin Definitions - POT
-const int effectPotPin1   = 21;
-const int effectPotPin2   = 20;
-const int effectPotPin3   = 19;
-const int effectPotPin4   = -1;
+const int effectPotPin1   = 23;
+const int effectPotPin2   = 22;
+const int effectPotPin3   = 21;
+const int effectPotPin4   = 20;
 
 // Pin Definitions - LED
-const int effectLedPin1    = 15;
-const int effectLedPin2    = 16;
-const int effectLedPin3    = 17;
-const int effectLedPin4    = -1;
+const int effectLedPin1    = 16;
+const int effectLedPin2    = 17;
+const int effectLedPin3    = 15;
+const int effectLedPin4    = 18;
 
 #endif
